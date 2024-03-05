62efd6cd7275b4932fbc4b48dee5ee39e7a8a269
Tom Laplace <tomlaplace0902@gmail.com>
2024-03-05 13:58:28 +0100

    wip - floor

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed = 1.0f;
     public float jumpForce = 5.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += speed * Time.deltaTime * Vector3.right;
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            GetComponent<Animator>().SetTrigger("Jump");
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
